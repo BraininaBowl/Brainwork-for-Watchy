2512ea25068a98462212d683494515473affe429
BraininaBowl <fransvb@gmail.com>
2022-07-30 21:00:59 +0200

    multi-face started

--- main.h
+++ main.h
@@ -1,23 +1,60 @@
+#ifndef FIRST
+  #define FIRST
+  // ** INIT **
+  bool light = true;
+  int face = 0;
+#endif
 
-#ifndef BAHN_H
-#define BAHN_H
+#ifndef MAIN_H
+#define MAIN_H
 
 #include <Watchy.h>
-#include "DIN_1451_Engschrift_Regular64pt7b.h"
-#include "DIN_1451_Engschrift_Regular12pt7b.h"
+#include "bahn_vis.h"
+#include "maze_vis.h"
 
 
 
 class WatchyBrain : public Watchy {
   using Watchy::Watchy;
   public:
-    // ** INIT **
-    bool light = true;
     void drawWatchFace();
     void drawBahn(bool light);
+    void drawMaze(bool light);
+    virtual void handleButtonPress();//Must be virtual in Watchy.h too
 };
 
 #include "bahn.h"
+#include "maze.h"
+
+void WatchyBrain::handleButtonPress() {
+  if (guiState == WATCHFACE_STATE) {
+    //Up and Down switch watch faces
+    uint64_t wakeupBit = esp_sleep_get_ext1_wakeup_status();
+    if (wakeupBit & UP_BTN_MASK) {
+      face += 1;
+    } else if (wakeupBit & DOWN_BTN_MASK) {
+      face -= 1;
+    } else if (wakeupBit & BACK_BTN_MASK) {
+      if (light == false) {
+        light = true;
+        //display.epd2.setDarkBorder(false);
+      } else {
+        light = false;
+        //display.epd2.setDarkBorder(true);
+      }
+    }
+    if (face < 0 ) {face = 1;}
+    if (face > 1 ) {face = 0;}
+    if (wakeupBit & (DOWN_BTN_MASK | UP_BTN_MASK | BACK_BTN_MASK)) {
+      //Face changed, show immediately
+      RTC.read(currentTime);
+      showWatchFace(true);
+      Watchy::handleButtonPress();
+    }
+    else Watchy::handleButtonPress();
+  } else Watchy::handleButtonPress(); //Watchy handles menus etc.
+}
+
 
 void WatchyBrain::drawWatchFace() {
 
@@ -28,8 +65,12 @@ void WatchyBrain::drawWatchFace() {
   if (currentTime.Hour == 00 && currentTime.Minute == 00) {
     sensor.resetStepCounter();
   }
-
-  drawBahn(light);
+  if (face == 0) {
+    drawBahn(light);  
+  } else {
+    drawMaze(light);
+  }
+  
 }
 
 #endif
---
+++ maze.h
@@ -0,0 +1,25 @@
+void WatchyBrain::drawMaze(bool light) {
+  const unsigned char *images [10] = {img0,img1,img2,img3,img4,img5,img6,img7,img8,img9};
+            
+      //drawbg
+      display.fillScreen(light ? GxEPD_BLACK : GxEPD_WHITE);
+      display.drawBitmap(0, 0, background, 200, 200, light ? GxEPD_WHITE : GxEPD_BLACK);
+
+      //draw time
+      display.fillRect(29,29,66,66, light ? GxEPD_BLACK : GxEPD_WHITE);
+      display.fillRect(29,107,66,66, light ? GxEPD_BLACK : GxEPD_WHITE);  
+      display.fillRect(107,29,66,66, light ? GxEPD_BLACK : GxEPD_WHITE);  
+      display.fillRect(107,107,66,66, light ? GxEPD_BLACK : GxEPD_WHITE); 
+                  
+      display.drawBitmap(29, 29, images[currentTime.Hour/10], 66, 66, light ? GxEPD_WHITE : GxEPD_BLACK);
+      display.drawBitmap(107, 29, images[currentTime.Hour%10], 66, 66, light ? GxEPD_WHITE : GxEPD_BLACK);
+                
+      display.drawBitmap(29, 107, images[currentTime.Minute/10], 66, 66, light ? GxEPD_WHITE : GxEPD_BLACK);
+      display.drawBitmap(107, 107, images[currentTime.Minute%10], 66, 66, light ? GxEPD_WHITE : GxEPD_BLACK);
+     
+      // draw battery
+      float batt = (getBatteryVoltage()-3.3)/0.9;
+      if (batt > 0) {
+       display.fillRect(0,185,2,12*batt,light ? GxEPD_BLACK : GxEPD_WHITE);
+      }   
+}
